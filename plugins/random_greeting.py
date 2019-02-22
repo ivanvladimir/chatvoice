7f53e329824d42f92bf792f74de61ee90bfff052
Eduardo <eduardoverd_@hotmail.com>
2019-02-22 13:18:40 -0600

    primer commit

--- plugins/random_greeting.py
+++ plugins/random_greeting.py
@@ -9,6 +9,9 @@ import random
 def execute(*args):
     var=args[0]
     opts=args[1]
-    msg = random.choice(['hola','buen día','chevere']+opts)
+    msg = random.choice(['Hola','Buenas tardes','Buen dia']+opts)
+    #msg_noche = random.choice(['Hola','Buenas noches']+opts)     lista de saludo para horario de noche
+    #msg_noche = random.choice(['Hola','Buenos dias']+opts)     lista de saludo para horario de la ma;ana
+    #descubrir como pedir la hora del sistema para definir de que lista tomara el saludo
     return 'set_slot {0} "{1}"'.format(var,msg)
 
