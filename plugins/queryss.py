04e41ddd24398f2543d8e7ec64f10d2b80e4d386
janine <ljaninegb@gmail.com>
2019-05-02 12:23:03 -0500

    consultas a  la bd de platillos

---
+++ plugins/queryss.py
@@ -0,0 +1,28 @@
+import numpy as np
+import sqlite3
+connection_platillos = sqlite3.connect("/home/ljanine/repo/chatvoice/conversations/platillos.db")
+cursor_platillo = connection_platillos.cursor()
+
+horario = 'comida'
+picante  =  np.random.choice(2, 1, replace=True, p=[0.2,0.8])
+caldo = np.random.choice(2, 1, replace=True, p=[0.2,0.8])
+mar = np.random.choice(2, 1, replace=True, p=[0.2,0.8])
+dulce = np.random.randint(0,1,1)
+
+picante =1
+caldo = 0
+mar = 1
+dulce = 0
+
+consulta = cursor_platillo.execute('SELECT platillo FROM platillos WHERE horario=? or picante=? or caldo=? or mar=? ', (horario, picante, caldo, mar))
+msg = cursor_platillo.fetchone()
+
+
+
+#print(type(msg))
+print(msg)
+#print(msg[1])
+print(picante)
+print(caldo)
+print(mar)
+print(dulce)
