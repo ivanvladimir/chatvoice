f5de51ebe2df74f0bf4d91920cd722dc07cbcd86
janine <ljaninegb@gmail.com>
2019-05-23 18:19:52 -0500

    Query bitacora

--- plugins/bitacora.py
+++ plugins/bitacora.py
@@ -7,7 +7,7 @@ from datetime import datetime, timedelta
 
 
 
-with open('/home/krmn/Documentos/IIMAS/chatvoice/conversations/kb.db','r') as f:
+with open('/home/ljanine/repo/chatvoice/conversations/kb.db','r') as f:
     data =f.readlines()
 for x in data:
     json.dumps(x)
@@ -17,32 +17,43 @@ weight_ini=float(y["weight_ini"])
 ahora=datetime.now().date()
 nombre=y["name"]
 
-connection_seguimiento = sqlite3.connect("/home/krmn/Documentos/IIMAS/chatvoice/conversations/seguimiento.db")
+connection_seguimiento = sqlite3.connect("/home/ljanine/repo/chatvoice/conversations/seguimiento.db")
 cursor_seguimiento = connection_seguimiento.cursor()
-
-
+peso1=0
+peso2=0
+peso3=0
+print(nombre)
 #cursor_seguimiento . execute ( "INSERT INTO objetivo (nombre, fecha, peso) VALUES (?,?,?)", (nombre, ahora, weight_ini))
-cursor_seguimiento . execute ( "INSERT INTO objetivo (nombre, fecha, peso) SELECT ?, ?, ? FROM objetivo WHERE NOT EXISTS(SELECT * FROM objetivo where nombre=? and fecha=?)", (nombre, ahora, weight_ini, nombre, ahora))
-#print ("consulta")
-consulta = cursor_seguimiento . execute ( "select fecha from objetivo where post_id=2 and nombre=?", (nombre,))
-m = cursor_seguimiento.fetchone()
-ms = m[0]
-#print (ms)
+cursor_seguimiento . execute ( "INSERT INTO objetivo (nombre, fecha, peso, peso1, peso2, peso3) VALUES(?,?,?,?,?,?) WHERE NOT EXISTS(SELECT * FROM objetivo where nombre=? and fecha=?)", (nombre, ahora, weight_ini, peso1, peso2, peso3,nombre,ahora))
+consulta = cursor_seguimiento . execute ( "Select fecha from objetivo where nombre=?",(nombre,))
+m = consulta.fetchone()
+ms = m[ 0 ]
 
 formato = "%Y-%m-%d"
 ms = datetime.strptime(ms, formato).date()
-print (ms)
 
 contador = ms + timedelta(days=0)
 print (contador)
-
 if contador == ahora:
 	print ("Hola {} , recuerdas que dariamos seguimiento a tu peso,  ¡pues hoy es el dia de checarnos!" .format(nombre))
-	weight_goal = input('¿Puedes decirme cuanto pesas el día de hoy?\n')
-	cursor_seguimiento . execute ( "INSERT INTO objetivo (nombre, fecha, peso) VALUES (?,?,?)", (nombre, ahora, weight_goal))
+	weight_actual = input('¿Puedes decirme cuanto pesas el día de hoy?\n')
+	cursor_seguimiento . execute ( "UPDATE objetivo SET peso1 = ? where nombre=?",  (weight_actual, nombre, ))
 
-	
 
+
+contador2 = ms + timedelta(days=1)
+print (contador2)
+if contador2 == ahora:
+    print ("Hola {} , recuerdas que dariamos seguimiento a tu peso,  ¡pues hoy es el dia de checarnos!" .format(nombre))
+    weight_actual1 = input('¿Puedes decirme cuanto pesas el día de hoy?\n')
+    cursor_seguimiento . execute ( "UPDATE objetivo SET peso2 = ? where nombre=?", (weight_actual1, nombre, ))
+
+contador3 = ms + timedelta(days=2)
+print (contador3)
+if contador3 == ahora:
+    print ("Hola {} , recuerdas que dariamos seguimiento a tu peso,  ¡pues hoy es el dia de checarnos!" .format(nombre))
+    weight_actual2 = input('¿Puedes decirme cuanto pesas el día de hoy?\n')
+    cursor_seguimiento . execute ( "UPDATE objetivo SET peso3 = ? where nombre=?",  (weight_actual2, nombre, ))
 	
 connection_seguimiento.commit()
 cursor_seguimiento . close ()
