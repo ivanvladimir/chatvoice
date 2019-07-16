a4962a4c53bd6ed68e418be00998aef13f408979
Ivan Vladimir <ivanvladimir@gmail.com>
2019-07-16 11:21:47 -0500

    Componiendo el chatbot

--- plugins/random_greeting.py
+++ plugins/random_greeting.py
@@ -9,6 +9,6 @@ import random
 def execute(*args):
     var=args[0]
     opts=args[1]
-    msg = random.choice(['hola','buen día','chevere']+opts)
+    msg = random.choice(['hola','buen día','chevere','buenas tardes']+opts)
     return 'set_slot {0} "{1}"'.format(var,msg)
 
