8aca68853916b2168f381a9116cfa1807f6c8b35
Jesus Martinez <jesus.martinez@tripletres.com>
2019-07-23 20:39:48 -0500

    modificacion al random_greeting para tener mas saludos

--- plugins/random_greeting.py
+++ plugins/random_greeting.py
@@ -9,6 +9,6 @@ import random
 def execute(*args):
     var=args[0]
     opts=args[1]
-    msg = random.choice(['hola','buen día','chevere','qué tal']+opts)
+    msg = random.choice(['hola','buen día','qué tal']+opts)
     return 'set_slot {0} "{1}"'.format(var,msg)
 
