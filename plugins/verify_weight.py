8b8b20bddc135e82598c5581c1286ed5d6ea45bd
janine <ljaninegb@gmail.com>
2019-08-02 04:52:40 -0500

    adicion de la validacion del peso inicial

--- plugins/verify_weight.py
+++ plugins/verify_weight.py
@@ -23,3 +23,24 @@ def verify_goal(*goal_weight):
         #el peso no es un número
         msg = 2
     return 'set_slot {0} "{1}"'.format(var,str(msg))
+
+
+def verify_ini(*ini_weight):
+    var = ini_weight[0]
+    peso_inicial = ini_weight[1]
+    peso_objetivo = float(ini_weight[2])
+    #Quito los espacios
+    peso_inicial = peso_inicial.replace(" ","")
+    #valido si es una cadena numérica
+    if peso_inicial.isdigit():
+        peso_inicial = float(peso_inicial)
+        if (40 < peso_inicial) and (peso_inicial < 597) and (peso_inicial > peso_objetivo):
+            #el peso inicial está en un rango adecuado
+            msg = 1
+        else:
+            #el peso inicial no está en un rango adecuado
+            msg = 0
+    else:
+        #el peso no es un número
+        msg = 2
+    return 'set_slot {0} "{1}"'.format(var,str(msg))
