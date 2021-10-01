265c523eea1bbedd7f2e58351b4140d86786cd9a
Ivan Vladimir Meza Ruiz <ivanvladimir@gmail.com>
2021-10-01 03:46:02 -0500

    Adding demo api

---
+++ apis/demo/api.py
@@ -0,0 +1,16 @@
+from typing import Optional
+from fastapi import FastAPI, Request
+from pydantic import BaseModel
+
+class Info(BaseModel):
+    msg : str
+
+app = FastAPI()
+
+@app.get("/msg")
+def msg():
+    return {"msg": "This mesg comes from an api"}
+
+@app.post("/echo")
+def echo(info: Info):
+    return {"msg": info.msg}
