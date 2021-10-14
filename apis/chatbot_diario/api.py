732197783ac8155e8d99325958c385be2f734280
Ivan Vladimir Meza Ruiz <ivanvladimir@gmail.com>
2021-10-14 12:09:14 -0500

    API for chatbot diario

---
+++ apis/chatbot_diario/api.py
@@ -0,0 +1,21 @@
+from typing import Optional
+from fastapi import FastAPI, Request
+from pydantic import BaseModel
+
+class Info(BaseModel):
+    msg : str
+
+app = FastAPI()
+
+@app.post("/analyis")
+def echo(info: Info):
+    return {"msg": info.msg}
+
+@app.post("/sentiments")
+def echo(info: Info):
+    # Hacer el analis de sentiminetos
+    return {"msg": info.msg}
+
+@app.post("/events")
+def echo(info: Info):
+    return {"msg": info.msg}
