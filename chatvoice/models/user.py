5be40818e200a9f589193760920a4ea421ef98f2
Ivan Vladimir Meza Ruiz <ivanvladimir@gmail.com>
2026-06-25 01:00:21 -0600

    feat: add alembic and add kdb column

--- chatvoice/models/user.py
+++ chatvoice/models/user.py
@@ -1,7 +1,7 @@
 import uuid as uuid_pkg
 from datetime import UTC, datetime
 
-from sqlalchemy import DateTime, ForeignKey, String
+from sqlalchemy import DateTime, ForeignKey, String, JSON
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import Mapped, mapped_column
 from uuid6 import uuid7
@@ -31,3 +31,6 @@ class User(Base):
     is_verified: Mapped[bool] = mapped_column(default=False)
 
     tier_id: Mapped[int | None] = mapped_column(ForeignKey("tier.id"), index=True, default=None, init=False)
+
+    # Knodledge based for the user
+    kdb: Mapped[dict] = mapped_column(JSON, default=None, nullable=True)
